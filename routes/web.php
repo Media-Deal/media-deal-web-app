79e03ba6906819ff44636c04401759f7c61e868d
eddyblues13 <eddyblues13@gmail.com>
2024-09-09 03:13:38 -0700

    email verification

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\CustomAuthController;
 
 Route::get('/', function () {
     return view('home.homepage');
@@ -16,4 +18,12 @@ Route::get('/policies', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [CustomAuthController::class, 'index'])->middleware('user_auth')->name('home');
+// Route for displaying the verification page
+Route::get('/verify/{id}', [CustomAuthController::class, 'verify'])->name('verify');
+
+// Route for handling the verification code submission
+Route::post('/verify-code', [CustomAuthController::class, 'verifyCode'])->name('verify.code');
+
+// Route for resending the verification code
+Route::get('/resend-verification-code', [CustomAuthController::class, 'resendVerificationCode'])->name('resend.verification.code');
