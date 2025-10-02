5ee3c97b5aee78b423c468718e264e25d4ef886a
eto <eto.saviour80@gmail.com>
2025-10-02 14:25:21 +0100

    Feature/Media

--- app/Models/AdvertiserPaymentHistory.php
+++ app/Models/AdvertiserPaymentHistory.php
@@ -24,4 +24,17 @@ class AdvertiserPaymentHistory extends Model
     protected $casts = [
         'meta' => 'array', // Automatically cast 'meta' to and from an array
     ];
+
+
+     // Payment history belongs to an Advertiser
+    public function advertiser()
+    {
+        return $this->belongsTo(Advertiser::class, 'advertiser_id', 'id');
+    }
+
+    // Payment history belongs to a Media Organization
+    public function mediaOrganization()
+    {
+        return $this->belongsTo(MediaOrganization::class, 'media_id', 'id');
+    }
 }
