abe1d2735fe97a61550f3e589fe2e730390c78e9
eto <eto.saviour80@gmail.com>
2025-10-02 13:26:59 +0100

    Feature/Media

--- app/Http/Controllers/MediaOrganizationController.php
+++ app/Http/Controllers/MediaOrganizationController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Compliance;
-use App\Models\Adplacement;
+use App\Models\AdPlacement;
 use Illuminate\Http\Request;
 use App\Models\MediaOrganization;
 use App\Models\AdvertiserPaymentHistory;
