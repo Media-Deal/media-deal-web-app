3a2e1083561d095c56b9dcb274e65e2a350a368b
eto <eto.saviour80@gmail.com>
2025-10-02 13:37:10 +0100

    Feature/Media

--- resources/views/emails/notification.blade.php
+++ resources/views/emails/notification.blade.php
@@ -213,7 +213,7 @@
                                     <td align='left'
                                         style='color: #aaaaaa; font-size: 14px; font-family: "Work Sans", Calibri, sans-serif; line-height: 24px;'>
                                         <div style='line-height: 24px;'>
-                                            <span style='color: #333333;'>Copyright 2024 - All Rights Reserved</span>
+                                            <span style='color: #333333;'>Copyright 2025 - All Rights Reserved</span>
                                         </div>
                                     </td>
                                 </tr>
